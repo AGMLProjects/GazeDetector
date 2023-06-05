bffc2c84868a3cd978eef970d95647b81ea509ee
Lorenzo Venturelli <venturelli.lorenzo@outlook.it>
2023-06-05 22:10:03 +0200

    Changed optimizer

--- GRA_Net/split_dataset.py
+++ GRA_Net/split_dataset.py
@@ -1,5 +1,7 @@
-import os, shutil
+from os import listdir
+import cv2
 
+<<<<<<< Updated upstream
 if __name__ == "__main__":
     DATASET_PATH = "C:\\Users\\ventu\\Desktop\\Nuova cartella"
     # Move the images to the correct class directory
@@ -19,3 +21,19 @@ if __name__ == "__main__":
             except:
                 print(filename)
             
+=======
+DIR = "./Gender/"
+
+#for filename in listdir('C:/tensorflow/models/research/object_detection/images/train'):
+count = 0
+for filename in listdir(DIR):
+  count += 1
+  if filename.endswith(".jpg"):
+    print(DIR+filename)
+    #cv2.imread('C:/tensorflow/models/research/object_detection/images/train/'+filename)
+    cv2.imread(DIR+filename)
+      
+    if count % 1000.0 == 0:
+      n = input("Continuare? ")
+      count = 0
+>>>>>>> Stashed changes
