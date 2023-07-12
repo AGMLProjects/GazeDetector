7a7e0d98766a100e353e22cab98e09ae69775b0a
GabrieleRastelli <rastelli424@gmail.com>
2023-07-12 08:56:52 +0200

    Moved Evaluator

--- retrieval-net/evaluation/Evaluator.py
+++
@@ -1,4 +0,0 @@
-from RetrievalComponent import RetrievalComponent
-
-retrieval_component = RetrievalComponent()
-retrieval_component.evaluate("../data/small_dataset", 0.75, 200, "../data/eval_embeddings.csv")
