3c5016b61ecdff7719c382cbfec966cadc58d338
GabrieleRastelli <rastelli424@gmail.com>
2023-07-12 08:43:10 +0200

    Added retrieval component evaluation

---
+++ retrieval-net/evaluation/Evaluator.py
@@ -0,0 +1,4 @@
+from RetrievalComponent import RetrievalComponent
+
+retrieval_component = RetrievalComponent()
+retrieval_component.evaluate("../data/small_dataset", 0.75, 200, "../data/eval_embeddings.csv")
