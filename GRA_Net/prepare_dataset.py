9b10ea7ccb57b7abae778f38befc75f45c186e73
Lorenzo Venturelli <venturelli.lorenzo@outlook.it>
2023-07-12 11:01:09 +0200

    preprocess dataset

--- GRA_Net/prepare_dataset.py
+++ GRA_Net/prepare_dataset.py
@@ -1,29 +1,13 @@
 import numpy as np
-import pathlib, mlsocket, hashlib, json
+import pathlib, mlsocket, time, json
 import tensorflow as tf
+import tensorflow_datasets as tfds
 
-def get_label(file_path) -> int:
-	gender = int(tf.strings.split(file_path, "_")[1])
-	age = int(int(tf.strings.split(file_path, "_")[2]) / 10) * 10
-	signature = str(hashlib.sha1(str(file_path).encode("UTF-8")).hexdigest().upper())
-
-	return gender, age, signature
-
-def decode_image(img) -> tf.Tensor:
-	return tf.io.decode_jpeg(img, channels = 3)
-
-def process_path(file_path) -> tuple:
-	label = get_label(file_path = file_path)
-
-	img = tf.io.read_file(file_path)
-	img = decode_image(img = img)
-	
-	return img, label
 
 if __name__ == "__main__":
 
 	# Load the dataset path
-	data_directory = pathlib.Path("/homes/lventurelli/Resources/Gender")
+	data_directory = pathlib.Path("/home/lorenzo/GazeDetection/Dataset/Gender")
 	print("Dataset path: " + str(data_directory))
 
 	image_count = len(list(data_directory.glob("*.jpg")))
@@ -35,13 +19,26 @@ if __name__ == "__main__":
 	retrival_connector = mlsocket.MLSocket()
 	retrival_connector.connect(("127.0.0.1", 65432))
 
-	list_ds = list_ds.map(process_path, num_parallel_calls = tf.data.experimental.AUTOTUNE)
-
 	output = None
+	i = 0
+	y = 0
 
-	for image, label in list_ds:
-		x = image.numpy()
-		retrival_connector.send(x)
+	for file_path in list_ds:
+
+		while i < 10001:
+			i = i + 1
+			continue
+
+		try:
+			image = tf.io.decode_jpeg(tf.io.read_file(file_path), channels = 3)
+			image = image.numpy()
+			label = [int(tf.strings.split(file_path, "_")[1]), int(int(tf.strings.split(file_path, "_")[2]) / 10) * 10]
+		except Exception as e:
+			print(e)
+			continue
+
+		time.sleep(0.001)
+		retrival_connector.send(image)
 		metadata = retrival_connector.recv(1024)
 		metadata = json.loads(metadata.decode("utf-8"))
 
@@ -54,18 +51,23 @@ if __name__ == "__main__":
 				retrived_age = int(int(metadata["age"]) / 10) * 10
 				similarity = float(metadata["similarity"])
 			else:
-				embedding = tf.zeros(shape = (1), dtype = tf.int16)
+				embedding = tf.zeros(shape = (1), dtype = tf.uint8)
 				retrived_gender, retrived_age = -1, -1
 				similarity = -1
 
-		row = [x, label[0].numpy(), label[1].numpy(), label[2].numpy(), embedding, np.array(retrived_gender), np.array(retrived_age), np.array(similarity)]
+		row = [image, np.array(label, dtype = np.uint8), embedding, np.array([retrived_gender, retrived_age], dtype = np.uint8), np.array(similarity)]
 
 		if output is None:
 			output = np.array(row, dtype = object)
 		else:
-			output = np.append(output, np.array(row), axis = 0)
+			output = np.append(output, np.array(row, dtype = object), axis = 0)
 
-		break
+		if i % 1000 == 0 and i != 0:
+			y = i / 1000
+			np.savez_compressed(f"output{y}", output)
+			output = None
+			print(f"Saved output{y}.npy")
+		
+		i= i + 1
 
-	print(output.shape)
-	print(output[0])
+	
